57abd99cfafb6bfe9624db1685e8fab5b8c3e8c2
Spencer Ewall <spencerjewall@gmail.com>
2015-04-12 13:00:50 -0700

    big commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-var components = ['osc']
+function getSelectedTool() {
+	return document.getElementById("tool-select-btn").value;
+}
+
+
+
+window.onload = function() {
+	document.getElementById("tool-create-btn").onclick = function() {
+		
+	}
+
+	var canvas = document.getElementById("main-canvas");
+	var context = canvas.getContext("2d");
+
+
+}
